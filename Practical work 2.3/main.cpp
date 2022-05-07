a383e5d78aaebc3efd81e9baf577257c876c6a62
Илья Митин <iliya2001@mail.ru>
2022-05-08 01:03:15 +0300

    table

--- Practical work 2.3/main.cpp
+++ Practical work 2.3/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 #include "Stack.h"
 
 int getOperationWeight(char operation)
@@ -26,40 +27,47 @@ void deleteChar(std::string &str, char ch)
     str.erase(std::remove(str.begin(), str.end(), ch), str.end());
 }
 
-int main()
+void reverse(std::string str, std::string res, Stack st)
 {
-    Stack st;
-    int size;
-
-    std::string str, res = "";
-    getline(std::cin, str);
-
-    deleteChar(str, ' ');
-
+    char operations[][130] = {
+        "                                                      add to output line                                                       ",
+        "                                                       push into stack                                                         ",
+        " 1) attach the contents of the stack to the parenthesis in reverse order to the output line; 2) delete the bracket from stack. "};
+         
+    std::cout << 
+        "+======+===============================================================================================================================+=======+===============+\n"
+        "| Char |                                                           Operation                                                           | Stack | Result string |\n"
+        "+======+===============================================================================================================================+=======+===============+\n";
+        
+    int j;
+    std::string num;
     for (unsigned int i = 0; i < str.length(); i++)
     {
-        size = st.getSize();
-        switch(getOperationWeight(str[i]))
+        switch (getOperationWeight(str[i]))
         {
         case 1:
-            if(getOperationWeight(st.read()) == 1)
+            num = "";
+            if (getOperationWeight(st.read()) == 1)
                 res += st.pop();
-            if(getOperationWeight(st.read()) == 2)
+            if (getOperationWeight(st.read()) == 2)
                 while (st.getSize())
                 {
                     res += st.pop();
                     res += ' ';
                 }
+            j = 1;
             st.push(str[i]);
             break;
 
         case 2:
+            num = "";
             if (getOperationWeight(st.read()) == 2)
                 res += st.pop();
             st.push(str[i]);
             break;
 
         case 0:
+            num = "";
             if (str[i] == ')')
                 while (st.getSize())
                 {
@@ -71,19 +79,40 @@ int main()
                     res += st.pop();
                     res += ' ';
                 }
-            else 
+            else
                 st.push(str[i]);
             break;
 
         case -1:
+            j = 0;
             res += str[i];
-            if(getOperationWeight(str[i+1]) > -1)
+            num += str[i];
+            if (getOperationWeight(str[i + 1]) > -1)
                 res += ' ';
         }
-    }
 
+        if(num.length() && (getOperationWeight(str[i + 1]) > -1 || i == str.length()-1))
+            std::cout << "| " << std::left << std::setw(4) << num << " |" << operations[j] << "|\n";
+        else if (!num.length())
+            std::cout << "| " << std::left << std::setw(4) << str[i] << " |" << operations[j] << "|\n";
+    }                
+    std::cout <<
+        "+======+===============================================================================================================================+";
     res += ' ';
     res += st.pop();
+}
+
+int main()
+{
+    Stack st;
+    int size;
+
+    std::string str, res = "";
+    getline(std::cin, str);
+
+    deleteChar(str, ' ');
+
+    reverse(str, res, st);
 
     std::cout << res << "\n\n\n";
 
